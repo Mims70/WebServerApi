7a001628ba5495826f0810f7643a8f34a2234402
Mims70 <anyiammiracle7777@gmail.com>
2024-07-01 17:49:30 +0100

    modified hello.js

--- routes/hello.js
+++ routes/hello.js
@@ -6,10 +6,10 @@ const weatherService = require('../services/weatherService');
 router.get('/hello', async (req, res) => {
     const visitor_name = req.query.visitor_name;
     let client_ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
-    
+
     // If x-forwarded-for contains multiple IPs, take the first one
     if (client_ip.includes(',')) {
-        client_ip = client_ip.split(',')[0];
+        client_ip = client_ip.split(',')[0].trim();
     }
 
     console.log(`Client IP: ${client_ip}`); // Debugging IP detection
